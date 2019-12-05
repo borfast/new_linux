c9835a3788bdd28ae2e6ac61cb0fa5fd8315aee8
Raúl Pedro Fernandes Santos <borfast@gmail.com>
2019-12-05 19:36:06 +0000

    Fix Virtualbox setup

--- new_linux.sh
+++ new_linux.sh
@@ -61,7 +61,12 @@ sudo apt-get -y install make build-essential git gitg git-cola openjdk-11-jdk te
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $UBUNTU_CODENAME contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y virtualbox-6.0 virtualbox-guest-x11 virtualbox-ext-pack virtualbox-guest-additions-iso
+sudo apt-get install -y virtualbox-6.0 virtualbox-guest-x11
+
+curl -O https://download.virtualbox.org/virtualbox/6.0.14/Oracle_VM_VirtualBox_Extension_Pack-6.0.14.vbox-extpack
+VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-6.0.14.vbox-extpack
+
+curl -O https://download.virtualbox.org/virtualbox/6.0.14/VBoxGuestAdditions_6.0.14.iso
 
 
 #######
