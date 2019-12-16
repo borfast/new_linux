2222ffda6abc79994b5b9b73d6c4b37a80787f96
Raúl Pedro Fernandes Santos <borfast@gmail.com>
2019-12-16 16:24:09 +0000

    Add pnpm

--- new_linux.sh
+++ new_linux.sh
@@ -113,6 +113,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+sudo npm add -g pnpm
+
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get -y update && sudo apt-get -y install yarn
