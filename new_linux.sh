baffc5ad6f11cc5f4753f6ab9e1d5c060a41d833
Raúl Santos <borfast@gmail.com>
2021-06-19 14:40:51 +0100

    More changes...

--- new_linux.sh
+++ new_linux.sh
@@ -222,15 +222,15 @@ pushd $HOME/new_ubuntu_temp_and_a_random_string
 ##############
 # Virtualbox #
 ##############
-echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $UBUNTU_CODENAME contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y virtualbox-6.1 virtualbox-guest-x11
+# echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $UBUNTU_CODENAME contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+# wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install -y virtualbox-6.1 virtualbox-guest-x11
 
-curl -L -O https://download.virtualbox.org/virtualbox/6.1.12/Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack
-VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack
+# curl -L -O https://download.virtualbox.org/virtualbox/6.1.12/Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack
+# VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack
 
-curl -L -O https://download.virtualbox.org/virtualbox/6.1.12/VBoxGuestAdditions_6.1.12.iso
+# curl -L -O https://download.virtualbox.org/virtualbox/6.1.12/VBoxGuestAdditions_6.1.12.iso
 
 
 #######
