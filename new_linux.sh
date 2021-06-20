b69d18c0d22d7b44b5d91c47f73666862707f36d
Raúl Santos <borfast@gmail.com>
2021-06-20 13:28:14 +0100

    All done?

--- new_linux.sh
+++ new_linux.sh
@@ -264,15 +264,15 @@ ansible-playbook ansible-playbook.yml --ask-become-pass
 #######################
 # Node, pnpm and yarn #
 #######################
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
-sudo npm add -g pnpm
+# sudo npm add -g pnpm
 
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get -y update && sudo apt-get -y install yarn
-echo "export PATH=\"$(yarn global bin):\$PATH\"" >> .zshrc
+# curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt-get -y update && sudo apt-get -y install yarn
+# echo "export PATH=\"$(yarn global bin):\$PATH\"" >> .zshrc
 
 ########
 # Java #
@@ -283,32 +283,39 @@ echo "export PATH=\"$(yarn global bin):\$PATH\"" >> .zshrc
 #################################################
 # MailHog - https://github.com/mailhog/MailHog/ #
 #################################################
-curl -L -o $HOME/progs/bin/mailhog https://github.com/mailhog/MailHog/releases/download/v1.0.1/MailHog_linux_amd64
-chmod u+x $HOME/progs/bin/mailhog
+# curl -L -o $HOME/progs/bin/mailhog https://github.com/mailhog/MailHog/releases/download/v1.0.1/MailHog_linux_amd64
+# chmod u+x $HOME/progs/bin/mailhog
 
 
 ###########################
 # Studio 3T (for Mongodb) #
 ###########################
-curl -o ./studio3t.tar.gz https://download.studio3t.com/studio-3t/linux/2020.7.1/studio-3t-linux-x64.tar.gz &&
-tar -C $HOME/progs/ -xzf studio3t.tar.gz
-rm -rf ./studio3t.tar.gz
+# curl -o ./studio3t.tar.gz https://download.studio3t.com/studio-3t/linux/2020.7.1/studio-3t-linux-x64.tar.gz &&
+# tar -C $HOME/progs/ -xzf studio3t.tar.gz
+# rm -rf ./studio3t.tar.gz
 
 
 
 #######################################################
 ## ZShell #
 #######################################################
-sudo apt-get install fonts-powerline zsh zsh-theme-powerlevel9k
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> $HOME/.zshrc
+# sudo apt-get install fonts-powerline zsh zsh-theme-powerlevel9k
+# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> $HOME/.zshrc
 
 
 ## Clean up
-echo "Cleaning Up" &&
+echo "Running special installers and cleaning up" &&
+# popd &&
+# rm -rf $HOME/new_ubuntu_temp_and_a_random_string &&
+# sudo apt-get -f install &&
+# sudo apt-get autoremove &&
+# sudo apt-get -y autoclean &&
+# sudo apt-get -y clean
+
+pushd ./temp &&
+./pyenv-installer &&
+python3 ./get-poetry.py &&
+./node-setup_14.sh &&
 popd &&
-rm -rf $HOME/new_ubuntu_temp_and_a_random_string &&
-sudo apt-get -f install &&
-sudo apt-get autoremove &&
-sudo apt-get -y autoclean &&
-sudo apt-get -y clean
+rm -rf ./temp
